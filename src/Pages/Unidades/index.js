83d28d2df7c104710694829cbb61a2d816ca99ce
Gabriel Beserra <gabriel.fernandesb@gmail.com>
2022-01-31 07:38:36 -0300

    feat: Table row displaying units

--- src/Pages/Unidades/index.js
+++ src/Pages/Unidades/index.js
@@ -1,3 +1,10 @@
+// Import React to use React.Children.ToArray
+// It helps to add the keys of elements in a map function
+import React from 'react'
+
+//Hooks
+import { useState, useEffect } from 'react'
+
 // Links
 import { Link } from 'react-router-dom'
 
@@ -12,26 +19,47 @@ import Button from '../../components/Button'
 // Styled-components
 import { ButtonContainer } from './styles'
 
+// Toast
+import { toast } from 'react-toastify'
+
+// API functions
+import { getData } from '../../utils/apiFunctions'
+
 function Unidades() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    // Get data from json server
+    async function handleData() {
+      try {
+        // Get all data from /unidades
+        const response = await getData('/unidades')
+        setData(response)
+      } catch (error) {
+        toast.error('Não foi possível buscar os dados!')
+      }
+    }
+    handleData()
+  }, [])
+
   return (
     <Container>
       <Menu current="unidades"></Menu>
       <MainSection title="Unidades">
         <Table>
-          <TableRow
-            id="156"
-            nickname="Painel 1"
-            place="Rua 5"
-            brand="Canadian"
-            model="155W"
-          />
-          <TableRow
-            id="1562"
-            nickname="Painel 2"
-            place="Rua 15"
-            brand="Risen"
-            model="100W"
-          />
+          {React.Children.toArray(
+            data.map(unit => {
+              return (
+                <TableRow
+                  id={unit.id}
+                  nickname={unit.nickname}
+                  place={unit.place}
+                  brand={unit.brand}
+                  model={unit.model}
+                />
+              )
+            })
+          )}
         </Table>
         <ButtonContainer>
           <Link to={`/cadastro`}>
